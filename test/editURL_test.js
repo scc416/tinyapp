19248c9acdad1ed443547aa0c3252aaa56314719
scc416 <siuc@protonmail.com>
2021-11-11 19:27:07 +0000

    update editURL_test.js as the helper function was updated

--- test/editURL_test.js
+++ test/editURL_test.js
@@ -1,5 +1,5 @@
 const { assert } = require("chai");
-const { urlHelperGenerator } = require("../helpers.js");
+const { urlHelperGenerator, uniqueVisitorsCounter } = require("../helpers.js");
 
 const testURLs = {
   b6UTxQ: {
@@ -16,15 +16,19 @@ const { editURL: editURLWithTestURLs } = urlHelperGenerator(testURLs);
 
 describe("#editURL", function() {
   it("should updated the longURL", function() {
-    editURLWithTestURLs("a1234F", "i3BoGr", "https://telegram.org/");
+    editURLWithTestURLs("i3BoGr", "https://telegram.org/");
     const expectedResult = {
       b6UTxQ: {
         longURL: "https://protonmail.com/",
-        userId: "a1234F"
+        userId: "a1234F",
+        visitorsRecord: [],
+        numOfUniqueVisitors: uniqueVisitorsCounter
       },
       i3BoGr: {
         longURL: "https://telegram.org/",
-        userId: "a1234F"
+        userId: "a1234F",
+        visitorsRecord: [],
+        numOfUniqueVisitors: uniqueVisitorsCounter
       }
     };
     assert.deepStrictEqual(testURLs, expectedResult);
