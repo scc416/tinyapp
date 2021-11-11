83a2e81abbbe3f89500d98be937853fa357c7364
scc416 <siuc@protonmail.com>
2021-11-11 00:04:57 +0000

    refactor test/getUserInfoById_test.js

---
+++ test/getUserInfoById_test.js
@@ -0,0 +1,54 @@
+const { assert } = require("chai");
+const { userHelperGenerator } = require("../helpers.js");
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+const { getUserInfoById: getUserInfoByIdWithTestUsers } = userHelperGenerator(testUsers);
+const { getUserInfoById: getUserInfoByIdWithNoUsers } = userHelperGenerator({});
+
+describe("#getUserInfoById", function() {
+  it("should return a user with valid user id", function() {
+    const user = getUserInfoByIdWithTestUsers("userRandomID");
+    const expectedUserInfo = {
+      id: "userRandomID",
+      email: "user@example.com",
+      password: "purple-monkey-dinosaur"
+    };
+    assert.deepStrictEqual(user, expectedUserInfo);
+  });
+
+  it("should return undefined with invalid user id", function() {
+    const result = getUserInfoByIdWithTestUsers("doNotExist");
+    const expectedResult = undefined;
+    assert.strictEqual(result, expectedResult);
+  });
+
+  it("should return undefined with no user in the database", function() {
+    const result = getUserInfoByIdWithNoUsers("user@example.com");
+    const expectedResult = undefined;
+    assert.strictEqual(result, expectedResult);
+  });
+
+  it("should return undefined with an empty user Id", function() {
+    const result = getUserInfoByIdWithTestUsers("");
+    const expectedResult = undefined;
+    assert.strictEqual(result, expectedResult);
+  });
+
+  it("should return undefined with an undefined user id", function() {
+    const result = getUserInfoByIdWithTestUsers(undefined);
+    const expectedResult = undefined;
+    assert.strictEqual(result, expectedResult);
+  });
+});
